8d43d84f847c0ae90e26f90a1fef37e5582b5038
shua kang <jiuerrose@outlook.com>
2026-02-24 15:37:10 -0500

    add my script

---
+++ xh_dev/train_with_lerobot/train_smolvla.bash
@@ -0,0 +1,13 @@
+lerobot-train \
+  --policy.type=smolvla \
+  --policy.repo_id=${HF_USER}/libero-test \
+  --policy.load_vlm_weights=true \
+  --dataset.repo_id=HuggingFaceVLA/libero \
+  --env.type=libero \
+  --env.task=libero_10 \
+  --output_dir=./outputs/ \
+  --steps=100000 \
+  --batch_size=4 \
+  --eval.batch_size=1 \
+  --eval.n_episodes=1 \
+  --eval_freq=1000 \
